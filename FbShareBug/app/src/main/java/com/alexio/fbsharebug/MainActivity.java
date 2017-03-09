a8b273acfae700e76f94adb381db2d073dc4104e
Alexio <alexioamota@gmail.com>
2017-03-09 16:13:57 -0500

    initial project

---
+++ FbShareBug/app/src/main/java/com/alexio/fbsharebug/MainActivity.java
@@ -0,0 +1,13 @@
+package com.alexio.fbsharebug;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
